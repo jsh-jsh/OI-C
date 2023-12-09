91367a9b9a1ed2d51e362f3554ae2773a57f8117
jinshuhang <68993466+jsh-jsh@users.noreply.github.com>
2023-12-09 20:37:24 +0800

    Update DemoApp.vue

--- src/DemoApp.vue
+++ src/DemoApp.vue
@@ -27,7 +27,7 @@ export default defineComponent({
         initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
         editable: false,
         selectable: false,
-        selectMirror: true,
+        selectMirror: false,
         dayMaxEvents: true,
         weekends: true,
         select: this.handleDateSelect,
@@ -46,27 +46,27 @@ export default defineComponent({
     handleWeekendsToggle() {
       this.calendarOptions.weekends = !this.calendarOptions.weekends // update a property
     },
-    handleDateSelect(selectInfo) {
-      let title = prompt('Please enter a new title for your event')
-      let calendarApi = selectInfo.view.calendar
+    // handleDateSelect(selectInfo) {
+    //   let title = prompt('Please enter a new title for your event')
+    //   let calendarApi = selectInfo.view.calendar
 
-      calendarApi.unselect() // clear date selection
+    //   calendarApi.unselect() // clear date selection
 
-      if (title) {
-        calendarApi.addEvent({
-          id: createEventId(),
-          title,
-          start: selectInfo.startStr,
-          end: selectInfo.endStr,
-          allDay: selectInfo.allDay
-        })
-      }
-    },
-    handleEventClick(clickInfo) {
-      if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-        clickInfo.event.remove()
-      }
-    },
+    //   if (title) {
+    //     calendarApi.addEvent({
+    //       id: createEventId(),
+    //       title,
+    //       start: selectInfo.startStr,
+    //       end: selectInfo.endStr,
+    //       allDay: selectInfo.allDay
+    //     })
+    //   }
+    // },
+    // handleEventClick(clickInfo) {
+    //   if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    //     clickInfo.event.remove()
+    //   }
+    // },
     handleEvents(events) {
       this.currentEvents = events
     },
