24e2645d0cb7721e771b74ff85b1f134311fd2a4
jinshuhang <68993466+jsh-jsh@users.noreply.github.com>
2023-12-09 20:36:19 +0800

    Update event-utils.js

--- src/event-utils.js
+++ src/event-utils.js
@@ -5,20 +5,9 @@ let todayStr = new Date().toISOString().replace(/T.*$/, '') // YYYY-MM-DD of tod
 export const INITIAL_EVENTS = [
   {
     id: createEventId(),
-    title: 'All-day event',
-    start: todayStr,
-    editable: 'false'
-  },
-  {
-    id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
-  },
-  {
-    id: createEventId(),
     title: 'Codeforces Round 914 (Div. 2)',
     start: '20231210',
-    url: 'https://codeforces.com/contest/1904',
+    url: 'https://codeforces.com/contests/1904',
     editable: 'false'
   }
 ]
