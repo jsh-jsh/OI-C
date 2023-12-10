f874d791171a28bc40a2865371675e6e676a5dcf
jinshuhang <jshzz2009@foxmail.com>
2023-12-10 13:56:15 +0800

    test
    测试脚本

---
+++ src/basgc.py
@@ -0,0 +1,22 @@
+import requests
+import datetime
+import random
+from bs4 import BeautifulSoup
+import chardet
+import sys
+url = 'https://codeforces.com/api/contest.list?gym=false'
+user_agent = [
+    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+]
+ips = [
+  {'https':'180.96.63.186:9087','http':'118.190.95.35:9001'}
+] 
+response = requests.get(url)
+sys.stdout=open('output.txt', 'w')
+for i in range(10):
+  print("{")
+  print("  id: createEventId(),")
+  print("  title: \'",response.json()['result'][i]['name'],"\',")
+  print("  start: ",response.json()['result'][i]['startTimeSeconds'],",")
+  print("  url: 'https://codeforces.com/contests/",response.json()['result'][i]['id'],sep='')
+  print("},")
