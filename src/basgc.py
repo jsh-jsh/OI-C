724c4ec634ffa43456b3b45a540cb8e97363969a
jinshuhang <68993466+jsh-jsh@users.noreply.github.com>
2023-12-11 20:23:13 +0800

    Update basgc.py

--- src/basgc.py
+++ src/basgc.py
@@ -8,10 +8,8 @@ url = 'https://codeforces.com/api/contest.list?gym=false'
 user_agent = [
     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
 ]
-ips = [
-  {'https':'180.96.63.186:9087','http':'118.190.95.35:9001'}
-] 
 response = requests.get(url)
+print("jello")
 sys.stdout=open('output.txt', 'w')
 for i in range(10):
   print("{")
