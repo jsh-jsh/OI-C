e9b25b62d5d196572a4bfc6312fc5bf23485b764
jinshuhang <68993466+jsh-jsh@users.noreply.github.com>
2023-12-17 09:36:23 +0800

    Update basgc.py

--- src/basgc.py
+++ src/basgc.py
@@ -4,16 +4,19 @@ import random
 from bs4 import BeautifulSoup
 import chardet
 import sys
-url = 'https://codeforces.com/api/contest.list?gym=false'
+url = 'https://contests.sdutacm.cn/contests.json'
 user_agent = [
     "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
 ]
+ips = [
+  {'https':'180.96.63.186:9087','http':'118.190.95.35:9001'}
+] 
 response = requests.get(url)
-sys.stdout=open('src/output.txt', 'w')
-for i in range(10):
+sys.stdout=open('output.txt', 'w')
+for i in response.json():
   print("{")
   print("  id: createEventId(),")
-  print("  title: \'",response.json()['result'][i]['name'],"\',")
-  print("  start: ",response.json()['result'][i]['startTimeSeconds'],",")
-  print("  url: 'https://codeforces.com/contests/",response.json()['result'][i]['id'],sep='')
+  print("  title: \'",i['name'],"\',")
+  print("  start: ",i['start_time'],",")
+  print("  url: \'",i['link'],'\'',sep="")
   print("},")
